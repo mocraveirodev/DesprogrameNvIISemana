f74a572d274a2c7134d3827fe855ef7cf0ca9166
Monica Craveiro de Menezes <craveiromonica@gmail.com>
2019-02-07 09:41:44 -0200

    Aula 06/02/2019

---
+++ Aula 06.02/js/adiciona-paciente.js
@@ -0,0 +1,123 @@
+//Arquivo para tratativa de tudo relacionado ao formulário
+
+//Função para montar o Objeto Paciente a partir dos dados passados pelo Form
+obtemPaciente = (form) => {
+    let paciente = {
+        nome: form.nome.value, //O .value é referente ao value do campo input do form, ou seja, o conteúdo de texto do campo.
+        altura: form.altura.value,
+        peso: form.peso.value,
+        gordura: form.gordura.value,
+        // imc: imc
+        imc: calculaImc(form.peso.value, form.altura.value) //Usando a função para reaproveitamento do codigo com a logica do calculo do IMC.
+    };
+
+    return paciente; //Retorna o objeto montado
+}
+
+//Função para validar os dados dos pacientes
+validaPaciente = (paciente) => {
+    let erros = [];
+
+    (paciente.nome.length == 0) ? erros.push("O nome não pode ser em branco") : "";
+    (paciente.gordura.length == 0) ? erros.push("A gordura não pode ser em branco") : "";
+    (paciente.peso.length == 0) ? erros.push("O peso não pode ser em branco") : "";
+    (paciente.altura.length == 0) ? erros.push("A altura não pode ser em branco") : "";
+
+    !validaPeso(paciente.peso) ? erros.push("Peso é inválido") : "";
+    !validaAltura(paciente.altura) ? erros.push("Altura é inválida") : "";
+
+    return erros;
+}
+
+exibeMensagensDeErro = (erros) => {
+    let ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = ""
+    erros.forEach(function (erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+//Função para reaproveitamento do cógido com a logica para montar a td.
+montaTd = (dado, classe) => {
+    let td = document.createElement("td"); //Cria a td
+    td.classList.add(classe); //Adiciona a classe do CSS à td.
+    td.textContent = dado; //Adiciona o coneúdo de texto à td.
+
+    return td; //Retorna a td montada
+}
+
+//Função para reaproveitamento do cógido com a logica para montar a tr.
+montaTr = (paciente) => {
+    const pacienteTr = document.createElement("tr"); //Cria a tr.
+    pacienteTr.classList.add("paciente"); //Adiciona a classe do CSS à tr.
+
+    //Bloco comentado pois utilizamos a função montaTd() direto dentro do parâmetro do appendChild() onde montamos a tr para economizar variável.
+    // const nomeTd = montaTd(paciente.nome,"info-nome"); 
+    // const pesoTd = montaTd(paciente.peso,"info-peso");
+    // const alturaTd = montaTd(paciente.altura,"info-altura");
+    // const gorduraTd = montaTd(paciente.gordura,"info-gordura");
+    // const imcTd = montaTd(paciente.imc,"info-imc");
+
+    //Bloco comentado pois codigo foi reutilizado através da função montaTd().
+    // nomeTd.textContent = paciente.nome;
+    // pesoTd.textContent = paciente.peso;
+    // alturaTd.textContent = paciente.altura;
+    // gorduraTd.textContent = paciente.gordura;
+    // imcTd.textContent = paciente.imc;
+
+    //Bloco comentado pois utilizamos a função montaTd() direto dentro do parâmetro do appendChild() onde montamos a tr para economizar variável.
+    // pacienteTr.appendChild(nomeTd);
+    // pacienteTr.appendChild(pesoTd);
+    // pacienteTr.appendChild(alturaTd);
+    // pacienteTr.appendChild(gorduraTd);
+    // pacienteTr.appendChild(imcTd);
+
+    //Bloco onde usamos a função montaTd() direto dentro do parâmetro do appendChild() onde montamos a tr para economizar variável.
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr; //Retona a tr montada
+}
+
+
+// let botao = document.querySelector("#adicionar-paciente"); Comentado pois estamos selecionando o botão junto com o evento de click.
+
+//Evento de cliqk do botão.
+document.querySelector("#adicionar-paciente").addEventListener("click", (xuxu) => {
+    xuxu.preventDefault(); //Previne o comportamento padrão do formulário onde ao clicar recarrega a pagina e limpa os formulários.
+
+    const form = document.querySelector("#form-adiciona"); //Selecionando nosso formulario para buscarmos os dados que precisamos para adicionar na tabela.
+
+    let paciente = obtemPaciente(form);
+
+    //Bloco comentado pois o cógio foi reutilizado no momento em que criamos o Objeto.
+    // const nome = paciente.nome;
+    // const peso = paciente.peso;
+    // const altura = paciente.altura;
+    // const gordura = paciente.gordura;
+    // const imc = paciente.imc;
+
+    // const pacienteTr = montaTr(paciente); Comentado pois chamamos a função montaTr() direto no parâmetro do appendChild() para economizar variável.
+
+    //const tabela = document.querySelector("#tabela-pacientes"); Comentado pois estamos selecionando a tabela diretamente onde adicionamos a tr.
+
+    let erros = validaPaciente(paciente);
+
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+        return;
+    }
+
+    document.querySelector("#tabela-pacientes").appendChild(montaTr(paciente)); //Selecionando a tabela e Adicionando a tr criada na tabela.
+
+    let ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = ""
+
+    form.reset(); //Funçao responsável por apagar os dados do form após serem adicionados sem erros na tabela.
+
+});
